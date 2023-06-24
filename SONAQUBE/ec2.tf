beb4e75cb1f39ae627162466ae86a6f5d045ee32
AnthonyIfy <136186728+AnthonyIfy@users.noreply.github.com>
2023-06-24 09:59:09 -0500

    Update ec2.tf

--- SONAQUBE/ec2.tf
+++ SONAQUBE/ec2.tf
@@ -1,9 +1,8 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region    = "us-east-1"
-  profile   = "yusuf"
+  region     = "us-east-1"
+  profile = "jesmmet_profile" 
 }
-
 
 # create default vpc if one does not exit
 resource "aws_default_vpc" "default_vpc" {
@@ -97,7 +96,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.medium"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group_sonarqube.id]
-  key_name               = "devopskeypair"
+  key_name               = "Jesmmet"
 
   tags = {
     Name = "sonarqube_server"

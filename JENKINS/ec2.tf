455ded5fce7d9cd440f63a64e29bbf1d7a613ea5
Anthony <anthony.ifeanyi@outlook.com>
2023-06-23 22:04:05 -0500

    JENKINS

--- JENKINS/ec2.tf
+++ JENKINS/ec2.tf
@@ -1,9 +1,8 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region    = "us-east-1"
-  profile   = "default"
+  region     = "us-east-1"
+  profile = "jesmmet_profile" 
 }
-
 
 # create default vpc if one does not exit
 resource "aws_default_vpc" "default_vpc" {
